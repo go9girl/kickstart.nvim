a3eb1d3376f0a7c8c30df9ed698b05ee226f8479
go9girl <theresakramer@proton.me>
2026-03-09 08:33:16 +0100

    added stuff back 	new file:   flutter.lua 	modified:   init.lua 	new file:   keybinds.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,8 +1,102 @@
--- You can add your own plugins here or in other files in this directory!
+-- Yo can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+return {
 
----@module 'lazy'
----@type LazySpec
-return {}
+  {
+    'nvim-java/nvim-java',
+    config = function()
+      require('java').setup()
+      vim.lsp.enable 'jdtls'
+    end,
+  },
+  {
+    'denialofsandwich/sudo.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    config = true,
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function() require('lspsaga').setup {} end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
+  {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    'nvim-java/nvim-java',
+    config = function()
+      require('java').setup()
+      vim.lsp.enable 'jdtls'
+    end,
+  },
+
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    require('lazydev').setup {
+      library = { 'nvim-dap-ui' },
+    },
+  },
+  {
+    'RaafatTurki/hex.nvim',
+    -- defaults
+    config = function()
+      require('hex').setup {
+
+        -- cli command used to dump hex data
+        dump_cmd = 'xxd -g 1 -u',
+
+        -- cli command used to assemble from hex data
+        assemble_cmd = 'xxd -r',
+
+        -- functiiting done right. Shift address lines around, edit and delete bytes, it will all work itself out on :w.on that runs on BufReadPre to determine if it's binary or not
+        is_file_binary_pre_read = function()
+          -- logic that determines if a buffer contains binary data or not
+          -- must return a bool
+        end,
+
+        -- function that runs on BufReadPost to determine if it's binary or not
+        is_file_binary_post_read = function()
+          -- logic that determines if a buffer contains binary data or not
+          -- must return a bool
+        end,
+      }
+    end,
+  },
+
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function() require('bufferline').setup {} end,
+  },
+
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = true,
+    cmd = {
+      'KittyScrollbackGenerateKittens',
+      'KittyScrollbackCheckHealth',
+      'KittyScrollbackGenerateCommandLineEditing',
+    },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function() require('kitty-scrollback').setup() end,
+  },
+  require('dapui').setup(),
+}
